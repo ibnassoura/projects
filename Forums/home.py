de343167d8a8329ca4bd6298dbe24188286fd834
IbrahimNassoura <ibnassoura@yahoo.com>
2018-03-04 14:50:20 +0400

    Post now in index.html

--- Forums/home.py
+++ Forums/home.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template
-
+import models
+import stores
 
 app = Flask(__name__)
+
+post_store = stores.PostStore()
+post_store.add(models.Post("Life", "Life is alawys great"))
+post_store.add(models.Post("Sunshine", "Sunshine is amazing"))
 
 @app.route("/")
 @app.route("/index")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts = post_store.get_all())
 
 
 if __name__ == '__main__':
