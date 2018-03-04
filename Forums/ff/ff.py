a72446fd8b4599822dcf833bb7f70fd88ed9c642
IbrahimNassoura <ibnassoura@yahoo.com>
2018-03-04 11:21:08 +0400

    Final Flask Demo

--- Forums/ff/ff.py
+++ Forums/ff/ff.py
@@ -2,14 +2,14 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/index')
 def hello_world():
     return 'Hello World!'
 
 
-@app.route('/SayHello/<username>')
+@app.route('/SayHello2/<username>')
 def say_hello(username):
-    return "Hello" + username
+    return "Hello " + username
 
 if __name__ == '__main__':
     app.run()
